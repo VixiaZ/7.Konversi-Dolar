e3ebd5fb0b29735d021c195cef728933a9024a3d
Zulfikar Hidayatullah <45659177+VixiaZ@users.noreply.github.com>
2019-01-03 01:10:58 +0700

    Add files via upload

---
+++ Konversi Dolar ke rupiah.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+	int main()
+		{
+			float p1 = 3*2;
+			float p2 = 3*4;
+			float hasil;
+			
+			hasil = p1 / p2;
+			printf ("%3f");
+			p1/p2; 
+			return 0;
+		}
